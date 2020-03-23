d8577a85a49852a2cd8f3058968c31d44149f9b9
Jonas Gabriel <jonas@Jonass-iMac.local>
2020-03-23 12:28:07 -0300

     ↝ Cria rotas de login, logout e sign in

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('registro', 'AutenticadorControlador@registro');
+    Route::post('login', 'AutenticadorControlador@login');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', 'AutenticadorControlador@logout');
+    });
+});
+
+Route::get('produtos', 'ProdutosControlador@index')
+    ->middleware('auth:api');
