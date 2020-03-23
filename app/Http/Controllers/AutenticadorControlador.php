01463173cb74d5102633267cb82c0f675d2b83b4
Jonas Gabriel <jonas@Jonass-iMac.local>
2020-03-23 12:27:00 -0300

    :construction: Cria AutenticadorController

---
+++ app/Http/Controllers/AutenticadorControlador.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AutenticadorControlador extends Controller
+{
+    public function registro(Request $request)
+    {
+        //valida -> nome, email, senha
+        $request->validate([
+            "name" => "required|string",
+            "email" => "required|string|email|unique:users",
+            "password" => "required|string|confirmed"
+        ]);
+
+        // atribui ao User as credenciais vindas da requisição
+        $user = new User([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password)
+        ]);
+
+        $user->save();
+        return response()->json([
+            "res" => 'Usuário criado com sucesso'
+        ], 201);
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            "email" => "required|string|email",
+            "password" => "required|string"
+        ]);
+
+        $credenciais = [
+            "email" => $request->email,
+            "password" => $request->password
+        ];
+
+        if (!Auth::attempt($credenciais)) {
+            return response()->json([
+                "resp" => "Acesso negado"
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken("Token de acesso")->accessToken();
+
+        return response()->json([
+            "token" => $token
+        ], 200);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            "res" => "Deslogado com sucesso!"
+        ]);
+    }
+}
