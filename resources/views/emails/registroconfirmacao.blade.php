f9801bc58c76b431855f6d4ddb22839fd3c4c741
Jonas Gabriel <jonas@Jonass-iMac.local>
2020-03-23 22:10:46 -0300

    :lipstick: Cria view simples de confirmação

---
+++ resources/views/emails/registroconfirmacao.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h4>Seja bem vindo(a), {{$nome}}!</h4>
+        <p>Você acabou de acessar o sistema utilizando seu email: {{$email}}</p>
+        <p>Data / hora de acesso: {{$dataHora}}</p>
+        <p>Clique no link abaixo para confirmar seu email:</p>
+        <a href="{{$link}}">Clique aqui</a>
+    </body>
+</html>
